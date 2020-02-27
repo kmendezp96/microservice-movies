13b3d3a20f4263c7a5067b77e1955a64efb04d75
kmendezp <kmendezp@unal.edu.co>
2020-02-27 17:40:54 -0500

    local styles and scripts

--- services/web/src/index.js
+++ services/web/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom
+import "./styles.css"
+import "./axios.min.js"
 
 import App from './App.jsx';
 
